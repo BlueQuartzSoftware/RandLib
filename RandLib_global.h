51f3e13dd269328394d65dc7133bc392ca57cc32
Jared Duffey <jared.duffey@bluequartz.net>
2023-06-09 09:01:16 -0400

    Changed constants to be inline in RandLib_global.h
    * Moved conditional include to the top of the file
    
    Signed-off-by: Jared Duffey <jared.duffey@bluequartz.net>

--- RandLib_global.h
+++ RandLib_global.h
@@ -6,7 +6,11 @@
 #include <cmath>
 #include <string>
 
-constexpr long double MIN_POSITIVE = 1e-21l;
+#if !defined(INFINITY) || !defined(NAN)
+#include <limits>
+#endif
+
+inline constexpr long double MIN_POSITIVE = 1e-21l;
 
 template <typename T>
 using Pair = std::pair<T, T>;
@@ -41,63 +45,61 @@ using DoubleTriplet = Triplet<double>;
 using LongDoubleTriplet = Triplet<long double>;
 
 #ifndef INFINITY
-#include <limits>
-long double INFINITY = std::numeric_limits<long double>::infinity() l;
+inline constexpr long double INFINITY = std::numeric_limits<long double>::infinity();
 #endif
 
 #ifndef NAN
-#include <limits>
-long double NAN = std::numeric_limits<long double>::quiet_NaN() l;
+inline constexpr long double NAN = std::numeric_limits<long double>::quiet_NaN();
 #endif
 
 #ifndef M_E
-constexpr long double M_E = 2.71828182845904523536l;
+inline constexpr long double M_E = 2.71828182845904523536l;
 #endif
 
 #ifndef M_LOG2E
-constexpr long double M_LOG2E = 1.44269504088896340760l;
+inline constexpr long double M_LOG2E = 1.44269504088896340760l;
 #endif
 
 #ifndef M_LOG10E
-constexpr long double M_LOG10E = 0.43429448190325182765l;
+inline constexpr long double M_LOG10E = 0.43429448190325182765l;
 #endif
 
 #ifndef M_LN2
-constexpr long double M_LN2 = 0.69314718055994530942l;
+inline constexpr long double M_LN2 = 0.69314718055994530942l;
 #endif
 
 #ifndef M_LN10
-constexpr long double M_LN10 = 2.30258509299404568402l;
+inline constexpr long double M_LN10 = 2.30258509299404568402l;
 #endif
 
 #ifndef M_PI
-constexpr long double M_PI = 3.14159265358979323846l;
+inline constexpr long double M_PI = 3.14159265358979323846l;
 #endif
 
 #ifndef M_PI_2
-constexpr long double M_PI_2 = 1.57079632679489661923l;
+inline constexpr long double M_PI_2 = 1.57079632679489661923l;
 #endif
 
 #ifndef M_1_PI
-constexpr long double M_1_PI = 0.31830988618379067154l;
+inline constexpr long double M_1_PI = 0.31830988618379067154l;
 #endif
 
 #ifndef M_2_PI
-constexpr long double M_2_PI = 0.63661977236758134308l;
+inline constexpr long double M_2_PI = 0.63661977236758134308l;
 #endif
 
 #ifndef M_2_SQRTPI
-constexpr long double M_2_SQRTPI = 1.12837916709551257390l;
+inline constexpr long double M_2_SQRTPI = 1.12837916709551257390l;
 #endif
 
 #ifndef M_SQRT2
-constexpr long double M_SQRT2 = 1.41421356237309504880l;
+inline constexpr long double M_SQRT2 = 1.41421356237309504880l;
 #endif
 
 #ifndef M_SQRT1_2
-constexpr long double M_SQRT1_2 = 0.70710678118654752440l;
+inline constexpr long double M_SQRT1_2 = 0.70710678118654752440l;
 #endif
 
 #ifndef M_SQRT5
-constexpr long double M_SQRT5 = 2.23606797749978969640l;
+inline constexpr long double M_SQRT5 = 2.23606797749978969640l;
 #endif
